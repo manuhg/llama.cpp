b3447c7698ba2ab1f864adbe543dc4147451b4a1
Manu Hegde <manuhegdev@gmail.com>
2025-05-03 14:31:04 -0700

    update script to copy libs to release-produts dir

--- scripts/build-all-release.sh
+++ scripts/build-all-release.sh
@@ -2,3 +2,4 @@
 mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DLLAMA_METAL=on && make -j$(nproc)
 mkdir -p libs
 find . -type f -name "*.a" -exec cp "{}" libs/ \;
+cp libs/* ~/proj_tldr/tldr-dekstop/release-products
