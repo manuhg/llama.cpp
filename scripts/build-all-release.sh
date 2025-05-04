1a792df0a6bb8e2e7adcc80544536f12c58c6199
Manu Hegde <manuhegdev@gmail.com>
2025-05-03 18:46:25 -0700

    update cmake such that output copy script gets triggered after cmake build automatically

--- scripts/build-all-release.sh
+++ scripts/build-all-release.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DLLAMA_METAL=on && make -j$(nproc)
-mkdir -p libs
-find . -type f -name "*.a" -exec cp "{}" libs/ \;
-cp libs/* ~/proj_tldr/tldr-dekstop/release-products/libs
-cp -rv ../include/* ~/proj_tldr/tldr-dekstop/release-products/include
-cp -rv ../ggml/include/* ~/proj_tldr/tldr-dekstop/release-products/include
+../scripts/copy-release-products.sh
---
+++ scripts/copy-release-products.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -p libs
+find .. -type f -name "*.a" -exec cp "{}" libs/ \;
+
+mkdir -p ~/proj_tldr/tldr-dekstop/release-products/libs/llama.cpp
+mkdir -p ~/proj_tldr/tldr-dekstop/release-products/include/llama.cpp
+
+cp libs/* ~/proj_tldr/tldr-dekstop/release-products/libs/llama.cpp
+cp -rv ../include/* ~/proj_tldr/tldr-dekstop/release-products/include/llama.cpp
+cp -rv ../ggml/include/* ~/proj_tldr/tldr-dekstop/release-products/include/llama.cpp
