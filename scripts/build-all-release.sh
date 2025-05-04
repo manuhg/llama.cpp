9c538dc0acefa9d6d7a136846d0532cd070f1646
Manu Hegde <manuhegdev@gmail.com>
2025-05-03 17:29:35 -0700

    copy libs and includes to release-products dir

--- scripts/build-all-release.sh
+++ scripts/build-all-release.sh
@@ -2,4 +2,6 @@
 mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DLLAMA_METAL=on && make -j$(nproc)
 mkdir -p libs
 find . -type f -name "*.a" -exec cp "{}" libs/ \;
-cp libs/* ~/proj_tldr/tldr-dekstop/release-products
+cp libs/* ~/proj_tldr/tldr-dekstop/release-products/libs
+cp -rv ../include/* ~/proj_tldr/tldr-dekstop/release-products/include
+cp -rv ../ggml/include/* ~/proj_tldr/tldr-dekstop/release-products/include
